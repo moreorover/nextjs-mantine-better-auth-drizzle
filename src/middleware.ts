80233c4aadbf754b37d80e15cbde620b831ee49a
Martin <mselvenis@gmail.com>
2025-04-13 22:49:14 +0100

    feature: drizzle + better-auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,25 @@ import { getSessionCookie } from "better-auth/cookies";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
+  const url = new URL(request.url);
+  // Handle /two-factor access
+  if (url.pathname === "/two-factor") {
+    const twoFactorCookie = request.cookies.get("better-auth.two_factor");
+    if (!twoFactorCookie) {
+      return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+  }
+
   const cookies = getSessionCookie(request);
+
+  // Handle /profile access
   if (!cookies) {
     return NextResponse.redirect(new URL("/", request.url));
   }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/profile"],
+  matcher: ["/profile", "/two-factor"],
 };
