ee4ecf62a6424546517867939730850e347831bf
Martin <mselvenis@gmail.com>
2025-04-15 22:28:49 +0100

    feature: trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { ColorSchemesSwitcher } from "@/components/color-schemes-switcher";
 import { HydrateClient, api } from "@/trpc/server";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
 	void api.some.helloPublic.prefetch();
 	return (
--- src/components/color-schemes-switcher.tsx
+++ src/components/color-schemes-switcher.tsx
@@ -4,27 +4,41 @@ import { LoaderSkeleton } from "@/components/loader-skeleton";
 import { api } from "@/trpc/react";
 import { Button, Group, useMantineColorScheme } from "@mantine/core";
 import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export function ColorSchemesSwitcher() {
 	return (
-		<Suspense fallback={<LoaderSkeleton />}>
-			<ColorSuspenseSwitcher />
-		</Suspense>
+		<>
+			<Suspense fallback={<LoaderSkeleton />}>
+				<ErrorBoundary fallback={<p>Error</p>}>
+					<ColorSuspenseSwitcher />
+				</ErrorBoundary>
+			</Suspense>
+			<Suspense fallback={<LoaderSkeleton />}>
+				<ErrorBoundary fallback={<p>Error</p>}>
+					<PrivateButton />
+				</ErrorBoundary>
+			</Suspense>
+		</>
 	);
 }
 
 function ColorSuspenseSwitcher() {
 	const { setColorScheme, clearColorScheme } = useMantineColorScheme();
 	const [publicData] = api.some.helloPublic.useSuspenseQuery();
-	// const [privateData] = api.some.helloPrivate.useSuspenseQuery();
 	return (
 		<Group>
 			<Button>{publicData}</Button>
-			{/*<Button>{privateData}</Button>*/}
+
 			<Button onClick={() => setColorScheme("light")}>Light</Button>
 			<Button onClick={() => setColorScheme("dark")}>Dark</Button>
 			<Button onClick={() => setColorScheme("auto")}>Auto</Button>
 			<Button onClick={clearColorScheme}>Clear</Button>
 		</Group>
 	);
+}
+
+function PrivateButton() {
+	const [privateData] = api.some.helloPrivate.useSuspenseQuery();
+	return <Button>{privateData}</Button>;
 }
