cc7a97ca4579b38a9b7d2de925b627fa82d9531f
Martin <mselvenis@gmail.com>
2025-04-16 10:08:34 +0100

    feature: trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { ColorSchemesSwitcher } from "@/components/color-schemes-switcher";
+import { TrpcExample } from "@/components/trpc-example";
 import { HydrateClient, api } from "@/trpc/server";
+import { Container, Stack } from "@mantine/core";
 
 export const dynamic = "force-dynamic";
 
@@ -7,7 +9,12 @@ export default async function Page() {
 	void api.some.helloPublic.prefetch();
 	return (
 		<HydrateClient>
-			<ColorSchemesSwitcher />
+			<Container size="md">
+				<Stack>
+					<ColorSchemesSwitcher />
+					<TrpcExample />
+				</Stack>
+			</Container>
 		</HydrateClient>
 	);
 }
--- src/components/color-schemes-switcher.tsx
+++ src/components/color-schemes-switcher.tsx
@@ -1,44 +1,28 @@
 "use client";
 
 import { LoaderSkeleton } from "@/components/loader-skeleton";
-import { api } from "@/trpc/react";
 import { Button, Group, useMantineColorScheme } from "@mantine/core";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
 export function ColorSchemesSwitcher() {
 	return (
-		<>
-			<Suspense fallback={<LoaderSkeleton />}>
-				<ErrorBoundary fallback={<p>Error</p>}>
-					<ColorSuspenseSwitcher />
-				</ErrorBoundary>
-			</Suspense>
-			<Suspense fallback={<LoaderSkeleton />}>
-				<ErrorBoundary fallback={<p>Error</p>}>
-					<PrivateButton />
-				</ErrorBoundary>
-			</Suspense>
-		</>
+		<Suspense fallback={<LoaderSkeleton />}>
+			<ErrorBoundary fallback={<p>Error</p>}>
+				<ColorSuspenseSwitcher />
+			</ErrorBoundary>
+		</Suspense>
 	);
 }
 
 function ColorSuspenseSwitcher() {
 	const { setColorScheme, clearColorScheme } = useMantineColorScheme();
-	const [publicData] = api.some.helloPublic.useSuspenseQuery();
 	return (
 		<Group>
-			<Button>{publicData}</Button>
-
 			<Button onClick={() => setColorScheme("light")}>Light</Button>
 			<Button onClick={() => setColorScheme("dark")}>Dark</Button>
 			<Button onClick={() => setColorScheme("auto")}>Auto</Button>
 			<Button onClick={clearColorScheme}>Clear</Button>
 		</Group>
 	);
-}
-
-function PrivateButton() {
-	const [privateData] = api.some.helloPrivate.useSuspenseQuery();
-	return <Button>{privateData}</Button>;
 }
